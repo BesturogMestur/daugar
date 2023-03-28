3e4f972eac852e245c3dd36130c1221edc400ac5
BesturogMestur <birgirsig02@gmail.com>
2023-03-28 11:28:02 +0000

    þá er alli klassar komir

---
+++ src/main/java/com/example/draugar/Hreyfigeta.java
@@ -0,0 +1,8 @@
+package com.example.draugar;
+
+public class Hreyfigeta {
+
+    public int reknirit(double[] d, double[] stefna){
+        return (int)(Math.pow(d[0]-stefna[0],2)+Math.pow(d[0]-stefna[0],2));
+    }
+}
